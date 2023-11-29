9096ea8bcbb64ce3026beda0ceb4d91a175034dc
zce <w@zce.me>
2023-11-29 13:49:18 +0800

    test

--- scripts/chokidar.js
+++ scripts/chokidar.js
@@ -1,7 +1,8 @@
 import { mkdir, writeFile } from 'fs/promises'
 import chokidar from 'chokidar'
+import glob from 'fast-glob'
 
-const watcher = chokidar.watch(['**/*.txt', '**/*.js'], {
+const watcher = chokidar.watch(['**/*.txt', '**/*.demo'], {
   ignored: /(^|[\/\\])[\._]./, // ignore dot & underscore files
   ignoreInitial: true, // ignore initial scan
   awaitWriteFinish: { stabilityThreshold: 50, pollInterval: 10 }
@@ -11,16 +12,25 @@ watcher.on('all', (event, path) => {
   console.log(event, path)
 })
 
-await writeFile('test.txt', 'Hello World')
-await writeFile('test2.txt', 'Hello World')
-await mkdir('test', { recursive: true })
-await writeFile('test/test2.txt', 'Hello World')
-await mkdir('foo/bar', { recursive: true })
-await writeFile('foo/bar/test2.txt', 'Hello World')
-
-await writeFile('test.js', 'console.log("Hello World")')
-
 setTimeout(async () => {
-  watcher.close()
-  process.exit(0)
+  await writeFile('test.txt', 'Hello World')
+  await writeFile('test2.txt', 'Hello World')
+  await mkdir('test', { recursive: true })
+  await writeFile('test/test2.txt', 'Hello World')
+  await mkdir('foo/bar', { recursive: true })
+  await writeFile('foo/bar/test2.txt', 'Hello World')
+
+  await mkdir('foo/baz', { recursive: true })
+  await writeFile('foo/baz/test.demo', 'console.log("Hello World")')
+
+  console.log('---------------------------')
+
+  console.log(await glob('**/*.txt'))
+
+  console.log('---------------------------')
+
+  setTimeout(() => {
+    watcher.close()
+    process.exit(0)
+  }, 1000)
 }, 1000)
